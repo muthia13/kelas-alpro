77fd9baf7fe0b22e275301aa6cc25b611885b454
muthia13 <146912775+muthia13@users.noreply.github.com>
2023-10-10 14:21:34 +0700

    Create operator_logika.c

---
+++ operator_logika.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+    int a = 4; // true
+    int b = 2; // true
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    // logika AND
+    printf("a && b = %d\n", a && b);
+
+    // logika OR
+    printf("a || b = %d\n", a || b);
+
+    // logika NOT
+    printf("!a = %d\n", !a);
+}
