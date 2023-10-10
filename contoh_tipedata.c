f821b4088e0ffb02db2b727f4ee1ae7ebbe1c1c8
muthia13 <146912775+muthia13@users.noreply.github.com>
2023-10-10 13:58:47 +0700

    Create contoh_tipedata.c

---
+++ contoh_tipedata.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int usia = 19;
+    float berat = 58.5;
+    double tinggi = 158.02;
+    char jenis_kelamin = 'P';
+
+    printf("Usia: %i tahun\n", usia);
+    printf("Tinggi: %.2f cm\n", tinggi);
+    printf("Berat: %.2f Kg\n", berat);
+    printf("Jenis kelamin: %c\n", jenis_kelamin);
+
+    return 0;
+}
