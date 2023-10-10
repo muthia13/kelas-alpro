f57859c4123b0c47b2299b853eb88b158e925c91
muthia13 <146912775+muthia13@users.noreply.github.com>
2023-10-10 14:16:50 +0700

    Create operator_aritmatikacampur.c

---
+++ operator_aritmatikacampur.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(){
+
+    int a, b;
+
+    printf("Input nilai a: ");
+    scanf("%i", &a);
+
+    printf("Input nilai b: ");
+    scanf("%i", &b);
+
+    printf("Hasil a + b: %i\n", a + b);
+    printf("Hasil a - b: %i\n", a - b);
+    printf("Hasil a * b: %i\n", a * b);
+    printf("Hasil a / b: %i\n", a / b);
+    printf("Hasil a % b: %i\n", a % b);
+
+}
