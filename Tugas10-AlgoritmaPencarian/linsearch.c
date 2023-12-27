c0c58fefa46096a84b822e6e8bbf50d5a7bee54f
muthia13 <146912775+muthia13@users.noreply.github.com>
2023-12-27 09:25:45 +0700

    Update linsearch.c

--- Tugas10-AlgoritmaPencarian/linsearch.c
+++ Tugas10-AlgoritmaPencarian/linsearch.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
+#include <time.h>
 #define SIZE 100
-int x;
+
 size_t linearSearch(const int array[], int key, size_t size);
+
 int main(void){
+    
     int a[SIZE];
-    for (size_t x = 0; x < SIZE; ++x)
-    {
-        a[x] = 2 * x;
+    
+    for (size_t x = 0; x < SIZE; ++x){
+        a[x] = 2*x + 1;
+        printf("a = %d\n", a[x]);
     }
+    
     printf("Enter integer search key: ");
     int searchKey;
     scanf("%d", &searchKey);
 
+    clock_t begin = clock();
     size_t index = linearSearch(a, searchKey, SIZE);
+    clock_t end = clock();
+    
     if (index != -1){
         printf("Found value at index %d\n", index);
-    }else{
+    }
+    else{
         puts("Value not found");
     }
 }
 
-    size_t linearSearch(const int array[], int key, size_t size){
-        for (size_t n = 0; n < SIZE; ++n)
-        {
-            if (array[n] == key)
-            {
+    size_t linearSearch(const int array[], int key, size_t size)
+{
+        for (size_t n = 0; n < SIZE; ++n){
+            if (array[n] == key){
                 return n;
             }
         }
